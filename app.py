fb08867c19240bb8e1b1e3889a38634c09bb3c62
nucleusfox <bountyfaye@gmail.com>
2020-02-26 21:12:25 +0300

    + Added model_1 and Flask server code to predict labels from model_1

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask
-from flask import request
-import json
+from flask import Flask, request
+import pickle
+import numpy
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        data = request.get_json()
-        predictions = {}
-        predictions['1'] = 'predicted_buy'
-        predictions['2'] = 'predicted_buy'
-        predictions['3'] = 'predicted_buy'
-        predictions['4'] = 'predicted_buy'
-        json_predictions = json.dumps(predictions, indent=1)
-        return json_predictions     
-    if request.method == 'GET':
-        return "You should be posting a model?!"
-    return "Flask Server how'd you get here?!"
 
-if __name__ == "__main__":
-	app.run(host='localhost', port=5000)
+@app.route('/', methods=['POST'])
+def predict():
+    request_json = request.get_json()
+
+    # Model 1 (Jane Ivanova)
+    loaded_model_1 = pickle.load(open('models/model_1.pkl', 'rb'))
+    max_records = 34
+    index = 0
+    testing_data = []
+    for frame in request_json:
+        index = index + 1
+        for keypoint in frame['keypoints']:
+            if (keypoint['part'] == 'rightWrist'):
+                testing_data.append(keypoint['position']['y'])
+        if index == max_records: break
+
+    norm_raw_data = (testing_data - numpy.mean(testing_data)) / (
+            numpy.max(testing_data - numpy.mean(testing_data)) - numpy.min(testing_data - numpy.mean(testing_data)))
+    diff_norm_raw_data = numpy.diff(norm_raw_data)
+    feature_vector = numpy.append(numpy.array([]), diff_norm_raw_data)
+    result_1 = loaded_model_1.predict([feature_vector])
+
+    print(result_1[0])
+    return {'1': result_1[0]}
